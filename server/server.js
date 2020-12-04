84dc499d49471de4c619bd82ff59325d9ad721b9
KimJeongin <mywnajsldkf@gmail.com>
2020-12-04 21:30:38 +0900

    re-commit

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ dotenv.config({
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT,
     console.log(`Server running mode on port ${PORT}`.red.underline.bold)
 );
