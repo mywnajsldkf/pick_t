73e3fc6b13187a7c7f079be6d4591b9b7d20d343
mywnajsldkf <mywnajsldkf@gmail.com>
2021-01-19 02:06:13 +0900

    edit : 카드뷰 리펙토링, 트레일러 등록 페이지 리펙토링 및 사진 업로드 기능 추가

--- client/app/src/main/java/com/example/pickt/AddCarActivity.java
+++ client/app/src/main/java/com/example/pickt/AddCarActivity.java
@@ -10,6 +10,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Adapter;
@@ -29,6 +30,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.w3c.dom.Text;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -37,9 +40,44 @@ import java.util.List;
 import java.util.Map;
 
 public class AddCarActivity extends AppCompatActivity {
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
+
+        imageButton = (ImageView) findViewById(R.id.addImageView);
+
+        // 사진 추가
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_GALLERY_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    imageButton.setImageBitmap(img);
+                } catch (Exception e) {
+
+                }
+
+            }else if(resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- client/app/src/main/java/com/example/pickt/HomeFragment.java
+++ client/app/src/main/java/com/example/pickt/HomeFragment.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.melnykov.fab.FloatingActionButton;
 
@@ -17,12 +19,28 @@ import com.melnykov.fab.FloatingActionButton;
 public class HomeFragment extends Fragment {
     ViewGroup viewGroup;
     FloatingActionButton addCar;
-    Button showList;
+    RecyclerView recyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         viewGroup = (ViewGroup)inflater.inflate(R.layout.fragment_home, container, false);
 
+        recyclerView = (RecyclerView) viewGroup.findViewById(R.id.carRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        MyCarData[] myCarData = new MyCarData[]{
+                new MyCarData(R.drawable.trailer1, "제이코 카라반 18피트", "광주 전남대점", "74,820원~"),
+                new MyCarData(R.drawable.trailer2, "코치맨 카라반 19피트 에이맥스 나노", "광주 일곡점", "83,210원~"),
+                new MyCarData(R.drawable.trailer1, "캠핑 트레일러 카라반", "광주 일곡점", "52,900원~"),
+                new MyCarData(R.drawable.trailer1, "블루밴 마이크로 XA", "광주 용봉점", "69,210원~")
+        };
+
+        MyCarAdapter myCarAdapter = new MyCarAdapter(myCarData, HomeFragment.this);
+        recyclerView.setAdapter(myCarAdapter);
+
+        // 차량 추가
         addCar = (FloatingActionButton) viewGroup.findViewById(R.id.addCar);
         addCar.setOnClickListener(new View.OnClickListener() {
             @Override
