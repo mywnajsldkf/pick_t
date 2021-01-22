25578153799e950489fc37a10d1366a624daa3f1
sunghyuki <sunghyuk1609@gmail.com>
2021-01-22 20:34:50 +0900

    init

--- client/app/src/main/java/com/example/pickt/OnChatRoomClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.pickt;
-
-import android.view.View;
-
-public interface OnChatRoomClickListener {
-    public void onItemClick(ChatRoomAdapter.ViewHolder holder, View view, int position);
-}
