46cb800383411e35c804775e7454e98a080aac3c
sunghyuki <sunghyuk1609@gmail.com>
2021-01-25 16:38:41 +0900

    LOG 확인

--- server/models/Trailer.js
+++ server/models/Trailer.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const trailerSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'user'
   },
 
   reportingDate: {
