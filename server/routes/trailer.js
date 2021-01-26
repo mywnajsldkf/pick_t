8758ffdb5ea85c951b0ca7400e2e49602fd281c9
sunghyuki <sunghyuk1609@gmail.com>
2021-01-26 14:21:20 +0900

    LOG 확인

--- server/routes/trailer.js
+++ server/routes/trailer.js
@@ -63,9 +63,7 @@ router.get('/trailers/:id', user_jwt, async(req, res, next) => {
   try {
     let trailer = await Trailer.findById(req.params.id);
 
-    console.log(trailer.length);
-    
-    if(trailer.length == 0) {
+    if(!trailer) {
       res.status(400).json({
         success: false,
         msg: 'trailerInfo not exists'
