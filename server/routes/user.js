544ca1b1fba04e7ce8b8ceb120ddd14214812cc5
sunghyuki <sunghyuk1609@gmail.com>
2021-01-27 20:28:19 +0900

    modify: 관심 목록 등록 해제 API 수정

--- server/routes/user.js
+++ server/routes/user.js
@@ -215,8 +215,6 @@ router.delete('/users/:id/likeLists', user_jwt, async(req, res, next) => {
 
     let likedTrailer = await User.findByIdAndUpdate(req.params.id, { $pull: { likeLists: { trailerId: req.body.trailerId } } });
 
-    likedTrailer = await User.findById(req.params.id).select('likeLists');
-
     if(!likedTrailer) {
       res.status(400).json({
         success: false,
