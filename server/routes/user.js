41b4266d68401852191289ca1b0db77cf30f6165
sunghyuki <sunghyuk1609@gmail.com>
2021-01-28 11:17:15 +0900

    modify: 관심 목록 가져오기 API 응답 수정

--- server/routes/user.js
+++ server/routes/user.js
@@ -213,7 +213,7 @@ router.get('/users/:id/likeLists', user_jwt, async(req, res, next) => {
       });
     }
 
-    let likedTrailer = await User.findById(req.params.id).select('-_id -username -email -password -nickname -phone -__v');
+    let likedTrailer = await User.findById(req.params.id).select('-_id -username -email -password -nickname -phone -__v -reservationLists');
 
     if(!likedTrailer) {
       res.status(400).json({
